14e7a15a14912d3e48396b0e8e6dfcb8d670cd1c
kharioki <antolisah@gmail.com>
2020-06-16 23:37:05 +0300

    routing with next/link

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function Index() {
-  return <h1>Details Page</h1>;
+  return (
+    <div>
+      <h1>Details Page</h1>
+      <Link as="/car/samantha" href="/[vehicle]/[person]">
+        <a>Navigate to Samantha's car</a>
+      </Link>
+    </div>
+  );
 }
