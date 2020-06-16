47f90640b45bea2f851bf1b3d52c70029a61c399
kharioki <antolisah@gmail.com>
2020-06-16 23:24:55 +0300

    using router props

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,3 +1,11 @@
-export default function Kharioki() {
-  return <h2>Kharioki's car</h2>;
+import { useRouter } from 'next/router';
+
+export default function Person() {
+  const router = useRouter();
+
+  return (
+    <h2>
+      {router.query.person}'s {router.query.vehicle}
+    </h2>
+  );
 }
