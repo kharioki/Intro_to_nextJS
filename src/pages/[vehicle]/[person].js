3202114300ac58ec3988cc5251dc4b8131154279
kharioki <antolisah@gmail.com>
2020-06-17 13:49:00 +0300

    getInitialProps with ctx query

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router';
 
-export default function Person() {
+export default function Person({ ownersList }) {
   const router = useRouter();
 
-  return (
-    <h2>
-      {router.query.person}'s {router.query.vehicle}
-    </h2>
-  );
+  return <pre>{JSON.stringify(ownersList, null, 4)}</pre>;
 }
+
+Person.getInitialProps = async ctx => {
+  const { query } = ctx;
+  const response = await fetch(
+    'http://localhost:4001/vehicles?ownerName=' + query.person
+  );
+  const ownersList = await response.json();
+  return { ownersList };
+};
