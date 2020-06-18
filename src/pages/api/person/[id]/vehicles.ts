062d403bb03da5b0c3ca879945816a9b07f51915
kharioki <antolisah@gmail.com>
2020-06-19 02:11:54 +0300

    fetch user's vehicles

--- src/pages/api/person/[id]/vehicles.ts
+++ src/pages/api/person/[id]/vehicles.ts
@@ -1,11 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
 
-export default function getAllVehiclesByPersonId(
+export default async function getAllVehiclesByPersonId(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.json({
-    byId: req.query.id,
-    message: 'getAllVehiclesByPersonId'
+  const db = await open({
+    filename: './mydb.sqlite',
+    driver: sqlite3.Database
   });
+
+  // fetch vehicles from db by person id
+  const vehicles = await db.all('SELECT * FROM vehicle where ownerId = ?', [
+    req.query.id
+  ]);
+
+  res.json(vehicles);
 }
