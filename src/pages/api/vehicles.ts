901308bb1959b4f3d90f198901d4ab3fd51e873b
kharioki <antolisah@gmail.com>
2020-06-18 23:56:11 +0300

    api methods, get vehicles, get people

---
+++ src/pages/api/vehicles.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function getAllVehicles(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    res.status(500).json({ message: 'Sorry we only accept GET requests' });
+  }
+
+  res.json({
+    hello: 'world',
+    method: req.method
+  });
+}
