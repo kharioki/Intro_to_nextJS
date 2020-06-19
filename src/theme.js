85ad26a879c0e507db764fa3fb06831ec9d0a947
kharioki <antolisah@gmail.com>
2020-06-19 05:50:13 +0300

    changing app background on theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { red, teal } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
@@ -14,7 +14,7 @@ const theme = createMuiTheme({
       main: red.A400
     },
     background: {
-      default: '#fff'
+      default: '#19857b'
     }
   }
 });
