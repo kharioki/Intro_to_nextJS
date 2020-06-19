d860fcfbe0bded5b2c233a62fe1695052d0b16e9
kharioki <antolisah@gmail.com>
2020-06-19 03:31:02 +0300

    updating user

--- migrations/001-helloworld.sql
+++ migrations/001-helloworld.sql
@@ -10,16 +10,20 @@ CREATE TABLE Vehicle (
   model TEXT,
   ownerId INTEGER REFERENCES Person(id)
 );
+--- insert into people
 INSERT INTO Person (name, email)
 values
   ('Tony', 'tony@stark.com');
 INSERT INTO Person (name, email)
 values
   ('June', 'june@stark.com');
+--- insert into vehicles
 INSERT INTO Vehicle (brand, model, ownerId)
 values('Mercedes', 'AMG', 1);
 INSERT INTO Vehicle (brand, model, ownerId)
 values('Hummer', 'H6', 2);
+INSERT INTO Vehicle (brand, model, ownerId)
+values('Mercedes', 'G-wagon', 1);
 -- Down
   DROP TABLE Person;
 DROP TABLE Vehicle;
--- src/pages/api/person/[id]/index.ts
+++ src/pages/api/person/[id]/index.ts
@@ -6,10 +6,23 @@ export default async function getPersonById(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const db = await open({
+  const db = await open<sqlite3.Database, sqlite3.Statement>({
     filename: './mydb.sqlite',
     driver: sqlite3.Database
   });
+
+  // update user
+  if (req.method === 'PUT') {
+    const statement = await db.prepare(
+      'UPDATE person SET name= ?, email= ?, where id= ?'
+    );
+    const result = await statement.run(
+      req.body.name,
+      req.body.email,
+      req.query.id
+    );
+    // result.finalize();
+  }
 
   // fetch person from db by req.query.id
   const person = await db.get('SELECT * FROM person where id = ?', [
